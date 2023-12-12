f09bbf21c66d2548f5b0b09a1c62c18fa28f6048
ghozy <ghozyarc12@gmail.com>
2023-12-12 20:54:44 +0300

    [CODE] resume dev

--- app/page.js
+++ app/page.js
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+import { BsBrowserChrome } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+import Image from "next/image";
+
+import pp from "../public/profil-pic.png";
+
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web4.png";
+
+const portofolioImages = [web1, web2, web3];
+
+const cardItems = [
+  {
+    gambar: design,
+    judul: "Design Bagus",
+    deskripsi: "Creating Elegant Design suited for needs design theory",
+    tools: ["canva", "figma"],
+  },
+  {
+    gambar: code,
+    judul: "Code Bagus",
+    deskripsi: "Creating Elegant Code suited for needs code theory",
+    tools: ["react", "laravel"],
+  },
+  {
+    gambar: consulting,
+    judul: "Consulting Bagus",
+    deskripsi: "Creating Consulting Design suited for needs design theory",
+    tools: ["consulting", "apacoba"],
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-white px-10 md:px-20 lg:px-40">
+      <section className=" min-h-screen text-slate-900">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-burtons">Portofolio Website</h1>
+          <ul className="flex items-center">
+            <li>
+              <BsBrowserChrome className="cursor-pointer text-2xl" />
+            </li>
+            <li>
+              <a
+                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                href="#"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="text-center p-10">
+          <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+            Ghozy Nouval Satya Prasaja
+          </h2>
+          <h3 className="text-2xl py-2 md:text-3xl">Fullstack Developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-800 md:text-lg max-w-xl mx-auto">
+            I recenly study in politeknik negeri malang and majoring in System
+            Informatic Bussiness
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillInstagram />
+        </div>
+        <div className="relative mx-auto bg-gradient-to-b mb-7 from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+          <Image src={pp} alt="pp" priority className="object-fill mt-5" />
+        </div>
+      </section>
+      <section className="text-slate-900">
+        <div>
+          <h3 className="text-3xl py-1">Services Offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-600">
+            I gave services such as fullstack using{" "}
+            <span className="text-teal-500">laravel</span> and frontend using
+            <span className="text-teal-500"> react</span>
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </div>
+        <div className="lg:flex gap-10">
+          {cardItems.map((newCard, i) => (
+            <Card
+              key={i}
+              gambar={newCard.gambar}
+              judul={newCard.judul}
+              deskripsi={newCard.deskripsi}
+              tools={newCard.tools}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="text-slate-900">
+        <div>
+          <h3 className="text-3xl py-1">Portofolio</h3>
+          <p className="text-md py-2 leading-8 text-gray-600">
+            I gave services such as fullstack using{" "}
+            <span className="text-teal-500">laravel</span> and frontend using
+            <span className="text-teal-500"> react</span>
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+        <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+          {portofolioImages.map((newPorto, i) => (
+            <PortofolioImagesCard key={i} gambar={newPorto} />
+          ))}
+        </div>
+      </section>
     </main>
-  )
+  );
+}
+
+function PortofolioImagesCard({ gambar }) {
+  return (
+    <div className="basis-1/3 flex-1">
+      <Image
+        src={gambar}
+        alt="porto"
+        className="rounded-lg object-cover w-full h-full"
+      />
+    </div>
+  );
+}
+
+function Card({ gambar, judul, deskripsi, tools }) {
+  return (
+    <div className="text-center shadow-lg p-10 rounded-xl my-10">
+      <Image
+        src={gambar}
+        width={100}
+        height={100}
+        alt="card"
+        className="mx-auto"
+      />
+      <h3 className="text-lg font-medium pt-8 pb-2">{judul}</h3>
+      <p className="py-2">{deskripsi}</p>
+      <h4 className="py-4 text-teal-500">Design Tools</h4>
+      {tools.map((newTools, i) => (
+        <p key={i} className="text-gray-800 py-1">
+          {newTools}
+        </p>
+      ))}
+    </div>
+  );
 }
